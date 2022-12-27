f115050390c6ad721fd2e51a2794056102ab9a6b
Jorge Ferrer <ferrerhernaez.2053116@studenti.uniroma1.it>
2022-12-27 17:28:16 +0100

    Fixing do login (no errors)

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -7,10 +7,10 @@ import (
 // Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
 	// Register routes
-	rt.router.GET("/fountains/", rt.wrap(rt.listFountains))
-	rt.router.POST("/fountains/", rt.wrap(rt.createFountain))
-	rt.router.PUT("/fountains/:id", rt.wrap(rt.updateFountain))
-	rt.router.DELETE("/fountains/:id", rt.wrap(rt.deleteFountain))
+	//rt.router.GET("/fountains/", rt.wrap(rt.listFountains))
+	rt.router.POST("/fountains/", rt.wrap(rt.doLogin))
+	//rt.router.PUT("/fountains/:id", rt.wrap(rt.updateFountain))
+	//rt.router.DELETE("/fountains/:id", rt.wrap(rt.deleteFountain))
 
 	// Special routes
 	rt.router.GET("/liveness", rt.liveness)
--- service/api/struct.go
+++ service/api/struct.go
@@ -1,13 +1,17 @@
 package api
 
-import "github.com/jorgeferrerhn/WASAPhoto/service/database" //este import da error
+import (
+	"regexp"
+
+	"github.com/jorgeferrerhn/WASAPhoto/service/database"
+) //este import da error
 
 // User struct represent a user in every data exchange with the external world via REST API. JSON tags have been
 // added to the struct to conform to the OpenAPI specifications regarding JSON key names.
 // Note: there is a similar struct in the database package. See User.FromDatabase (below) to understand why.
 type User struct {
-	ID   uint64  `json:"id"`
-	Name float64 `json:"name"`
+	ID   uint64 `json:"id"`
+	Name string `json:"name"`
 }
 
 // FromDatabase populates the struct with data from the database, overwriting all values.
@@ -15,19 +19,23 @@ type User struct {
 func (u *User) FromDatabase(user database.User) {
 	u.ID = user.ID
 	u.Name = user.Name
-	
 
-// ToDatabase returns the fountain in a database-compatible representation
+}
+
+// ToDatabase returns the user in a database-compatible representation
 func (u *User) ToDatabase() database.User {
 	return database.User{
-		ID:        u.ID,
-		Name:  u.Name,
-		
+		ID:   u.ID,
+		Name: u.Name,
 	}
 }
 
-// IsValid checks the validity of the content.  
+// IsValid checks the validity of the content.
 func (u *User) IsValid() bool {
 	m, err := regexp.MatchString("[a-zA-Z0-9]+", u.Name)
+
+	if err != nil {
+		return false
+	}
 	return m
 }
