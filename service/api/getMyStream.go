6b013cbb8170a06daf7dbc90c6ec8449ef839f63
Jorge Ferrer <ferrerhernaez.2053116@studenti.uniroma1.it>
2022-12-28 16:42:12 +0100

    Working on getMyStream

--- service/api/getMyStream.go
+++ service/api/getMyStream.go
@@ -1,11 +1,8 @@
 package api
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 	"strconv"
-	"strings"
 
 	"github.com/jorgeferrerhn/WASAPhoto/service/api/reqcontext"
 	"github.com/julienschmidt/httprouter"
@@ -33,48 +30,44 @@ func (rt *_router) getMyStream(w http.ResponseWriter, r *http.Request, ps httpro
 
 	//Searchs for the user to get the stream of photos
 
-	var user User
 	stringJson, err := rt.db.GetMyStream(i)
-	fmt.Println(stringJson)
-
-	//cast u.Photos to array
-	//photos := strings.Fields(stringJson)
-
-	fmt.Println("NOW AN EXAMPLE")
-	//example
-	example := `
-				{"PhotoId": 100000, 
-                          "Likes": ["JohnDoe","CarlSagan"],
-                          "Comments": [
-                              {"User":"JohnDoe","Comment":"Wow, nice trip!"},
-                              {"User":"CarlSagan", "Comment":"Amazing!"}],
-                          "Date":"2021-01-30T08:30:00Z",
-                          "Followers": ["JohnDoe","KenFollet"]
-                 },
-				 {"PhotoId": 100001, 
-                          "Likes": ["JohnDoe","CarlSagan"],
-                          "Comments": [
-                              {"User":"JohnDoe","Comment":"Wow, nice trip!"},
-                              {"User":"CarlSagan", "Comment":"Amazing!"}],
-                          "Date":"2021-01-30T08:30:00Z",
-                          "Followers": ["JohnDoe","KenFollet"]
-                 }
-				`
-
-	photos := strings.Fields(example)
-
-	fmt.Println(photos)
-
-	//cast string to JSON
-
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 
 	}
+
+	//cast u.Photos to array
+	//photos := strings.Fields(stringJson)
+
+	stringJson = `{"PhotoId": 100000,"Likes": ["JohnDoe","CarlSagan"],"Comments": [{"User":"JohnDoe","Comment":"Wow, nice trip!"},{"User":"CarlSagan", "Comment":"Amazing!"}],"Date":"2021-01-30T08:30:00Z","Followers": ["JohnDoe","KenFollet"]};{"PhotoId": 100001,"Likes": ["JohnDoe","CarlSagan"],"Comments": [{"User":"JohnDoe","Comment":"Wow, nice trip!"},{"User":"CarlSagan", "Comment":"Amazing!"}],"Date":"2021-01-30T08:30:00Z","Followers": ["JohnDoe","KenFollet"]}`
+
+	/*example
+	example := `
+		{"PhotoId": 100000,
+		"Likes": ["JohnDoe","CarlSagan"],
+		"Comments": [
+			{"User":"JohnDoe","Comment":"Wow, nice trip!"},
+			{"User":"CarlSagan", "Comment":"Amazing!"}],
+		"Date":"2021-01-30T08:30:00Z",
+		"Followers": ["JohnDoe","KenFollet"]
+			};
+			{"PhotoId": 100001,
+		"Likes": ["JohnDoe","CarlSagan"],
+		"Comments": [
+			{"User":"JohnDoe","Comment":"Wow, nice trip!"},
+			{"User":"CarlSagan", "Comment":"Amazing!"}],
+		"Date":"2021-01-30T08:30:00Z",
+		"Followers": ["JohnDoe","KenFollet"]
+			}
+		`
+	*/
+
+	//return the array on string format
+
 	defer r.Body.Close()
 
-	w.Header().Set("Content-Type", "application/json")
-	_ = json.NewEncoder(w).Encode(user)
+	w.Header().Set("Content-Type", "text/plain")
+	w.Write([]byte(stringJson))
 
 }
