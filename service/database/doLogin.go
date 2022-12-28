dc38a46f03ba6b2655ca548b51253f0fb792f04f
Jorge Ferrer <ferrerhernaez.2053116@studenti.uniroma1.it>
2022-12-28 15:39:53 +0100

    Finished getUserProfile

--- service/database/doLogin.go
+++ service/database/doLogin.go
@@ -1,19 +1,56 @@
 package database
 
+import (
+	"log"
+)
+
+var (
+	id         uint64
+	nameSearch string
+)
+
 func (db *appdbimpl) CreateUser(u User) (User, error) {
 
-	res, err := db.c.Exec(`INSERT INTO users (id, name,profilepic,followers,photos) VALUES (NULL, ?,?,?,?)`,
-		u.Name, 0, "[]", "[]")
+	//first we search the user. It should have a unique username, so we'll search for it
+	rows, err := db.c.Query(`select id, name from users where name=?`, u.Name)
+
 	if err != nil {
-		return u, err
+		log.Fatal(err)
 	}
 
-	lastInsertID, err := res.LastInsertId()
-	if err != nil {
-		return u, err
+	defer rows.Close()
+
+	for rows.Next() {
+		err := rows.Scan(&id, &nameSearch)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
 	}
 
-	u.ID = uint64(lastInsertID)
+	err = rows.Err()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if nameSearch == "" { //user has not been created before
+		res, err := db.c.Exec(`INSERT INTO users (id, name,profilepic,followers,photos) VALUES (NULL, ?,?,?,?)`,
+			u.Name, 0, "[]", "[]")
+		if err != nil {
+			return u, err
+		}
+
+		lastInsertID, err := res.LastInsertId()
+		if err != nil {
+			return u, err
+		}
+
+		u.ID = uint64(lastInsertID)
+
+	} else {
+		u.ID = id
+	}
 
 	return u, nil
 }
