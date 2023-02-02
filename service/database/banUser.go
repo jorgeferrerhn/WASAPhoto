2a3936cf19e693df7b67cc0e7f3af852d0739859
Jorge Ferrer <ferrerhernaez.2053116@studenti.uniroma1.it>
2023-02-02 12:08:16 +0100

    refactored banUser.go

--- service/database/banUser.go
+++ service/database/banUser.go
@@ -9,11 +9,6 @@ import (
 
 func (db *appdbimpl) BanUser(user1 User, user2 User) (User, error) {
 
-	var name1, followers1, banned1, photos1, name2 string
-	var profilePic1 int
-
-	//  We have to check if both users exist
-
 	//  search for the user that follows
 	rows, err := db.c.Query(`SELECT name,profilepic,followers,banned,photos FROM users WHERE id=?`, user1.ID)
 
@@ -25,7 +20,7 @@ func (db *appdbimpl) BanUser(user1 User, user2 User) (User, error) {
 
 	for rows.Next() {
 
-		err := rows.Scan(&name1, &profilePic1, &followers1, &banned1, &photos1)
+		err := rows.Scan(&user1.Name, &user1.ProfilePic, &user1.Followers, &user1.Banned, &user1.Photos)
 
 		if err != nil {
 			return user1, err
@@ -37,7 +32,7 @@ func (db *appdbimpl) BanUser(user1 User, user2 User) (User, error) {
 		return user1, err
 	}
 
-	if name1 == "" {
+	if user1.Name == "" {
 		return user1, errors.New("User 1 not found")
 	}
 
@@ -52,7 +47,7 @@ func (db *appdbimpl) BanUser(user1 User, user2 User) (User, error) {
 
 	for rows.Next() {
 
-		err := rows.Scan(&name2)
+		err := rows.Scan(&user2.Name)
 
 		if err != nil {
 			return user1, err
@@ -64,45 +59,30 @@ func (db *appdbimpl) BanUser(user1 User, user2 User) (User, error) {
 		return user1, err
 	}
 
-	if name2 == "" {
+	if user2.Name == "" {
 		return user1, errors.New("User 2 not found")
 	}
 
-	banned := strings.Contains(banned1, fmt.Sprint(user2.ID))
+	banned := strings.Contains(user1.Banned, fmt.Sprint(user2.ID))
 
 	if !banned {
 		var add string
-
-		newList := banned1[0 : len(banned1)-1]
-
-		if banned1 == "[]" {
+		newList := user1.Banned[0 : len(user1.Banned)-1]
+		if user1.Banned == "[]" {
 			add = fmt.Sprint(user2.ID) + "]"
-
 		} else {
 			add = "," + fmt.Sprint(user2.ID) + "]"
-
 		}
 		newList += add
-
 		user1.Banned = newList
-
-	} else {
-		user1.Banned = banned1 //  remains the same
-
 	}
 
-	user1.Name = name1
-	user1.ProfilePic = profilePic1
-	user1.Photos = photos1
-	user1.Followers = followers1
 	var res sql.Result
 	res, err = db.c.Exec(`UPDATE users SET name=?,profilepic=?,followers=?,banned=?,photos=? WHERE id=?`,
 		user1.Name, user1.ProfilePic, user1.Followers, user1.Banned, user1.Photos, user1.ID)
 	if err != nil {
 		return user1, errors.New("Error in " + fmt.Sprint(res))
 	}
-
-	// update list of followers
 
 	return user1, nil
 
