db72da8f485f47a8b5ab38fefe0852df29044a04
Jorge Ferrer <ferrerhernaez.2053116@studenti.uniroma1.it>
2023-02-05 17:09:14 +0100

    logging page updated

--- webui/src/views/doLogin.vue
+++ webui/src/views/doLogin.vue
@@ -6,6 +6,7 @@ export default {
       loading: false,
       users: [],
       token: 0,
+      loggedName: "",
       user: {}
     }
   },
@@ -40,6 +41,12 @@ export default {
       this.loading = false;
     },
 
+    logOut: async function() {
+      console.log("Logging out")
+      document.cookie = `token=0`; // reset the cookie
+      this.token = document.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1"); // update logged user
+    },
+
   },
   mounted() {
     this.refresh()
@@ -71,16 +78,36 @@ export default {
 
 
     <div class="card">
-      <div class="card-body">
 
-        <h3 class="h3">Introduce your username: </h3>
+      <template v-if="token == 0">
+        <h3 class="h3 mx-auto mt-3">Introduce your username: </h3>
+        <input v-model="name" class="text-center mx-auto my-auto mt-3 mb-3" placeholder=" Username">
 
-        <input v-model="name" placeholder=" Username">
+        <a href="javascript:" class="btn btn-primary mx-auto my-auto mb-3" @click="doLogin">Create a new user</a>
 
-        <p>Your token is: {{ token }} </p>
+      </template>
 
-        <a href="javascript:" class="btn btn-primary" @click="doLogin">Create a new user</a>
-      </div>
+      <template v-else>
+        <div class="card ">
+          <div class="card-header">
+
+          </div>
+          <div class="card ">
+            <div class="card-header">
+
+            </div>
+            <div class="card-body">
+              <h3 class="text-center">Welcome, {{ this.user["Name"]}}</h3>
+              <h5 class="text-center">Your token is: {{ token }}</h5>
+            </div>
+          </div>
+
+          <a href="javascript:" class="btn btn-primary mx-auto my-auto mb-3" @click="logOut">Log Out</a>
+
+        </div>
+      </template>
+
+
     </div>
 
 
