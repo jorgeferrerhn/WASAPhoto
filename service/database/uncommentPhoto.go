c42570db33171221136938124f73f7d5070ee2bc
Jorge Ferrer <ferrerhernaez.2053116@studenti.uniroma1.it>
2023-02-07 00:45:41 +0100

    changing res:= could be the problem

--- service/database/uncommentPhoto.go
+++ service/database/uncommentPhoto.go
@@ -22,16 +22,16 @@ func (db *appdbimpl) UncommentPhoto(c Comment, p Photo, u User) (Comment, Photo,
 
 	for rows.Next() {
 
-		err = rows.Scan(&u.Name, &u.ProfilePic, &u.Followers, &u.Banned, &u.Photos)
+		err2 := rows.Scan(&u.Name, &u.ProfilePic, &u.Followers, &u.Banned, &u.Photos)
 
-		if err != nil {
-			return c, p, u, err
+		if err2 != nil {
+			return c, p, u, err2
 		}
 	}
 
-	err = rows.Err()
-	if err != nil {
-		return c, p, u, err
+	err3 := rows.Err()
+	if err3 != nil {
+		return c, p, u, err3
 	}
 
 	if u.Name == "" {
@@ -40,29 +40,29 @@ func (db *appdbimpl) UncommentPhoto(c Comment, p Photo, u User) (Comment, Photo,
 
 	// then we search the comment ID. If it doesn't exist, we cannot uncomment the photo
 
-	rows2, err2 := db.c.Query(`select content,photoid,userid,date from comments where commentid=?`, c.ID)
+	rows2, err4 := db.c.Query(`select content,photoid,userid,date from comments where commentid=?`, c.ID)
 
-	if err2 != nil {
-		return c, p, u, err2
+	if err4 != nil {
+		return c, p, u, err4
 	}
 
 	defer rows2.Close()
 
 	for rows2.Next() {
 
-		err = rows2.Scan(&c.Content, &c.PhotoId, &c.UserId, &c.Date)
+		err5 := rows2.Scan(&c.Content, &c.PhotoId, &c.UserId, &c.Date)
 
-		if err != nil {
-			return c, p, u, err
+		if err5 != nil {
+			return c, p, u, err5
 		}
 
 	}
 
 	p.ID = c.PhotoId // After searching
 
-	err = rows2.Err()
-	if err != nil {
-		return c, p, u, err
+	err6 := rows2.Err()
+	if err6 != nil {
+		return c, p, u, err6
 	}
 
 	if c.Content == "" || c.PhotoId == 0 || c.UserId == 0 {
@@ -70,27 +70,27 @@ func (db *appdbimpl) UncommentPhoto(c Comment, p Photo, u User) (Comment, Photo,
 	}
 
 	// lastly, we need to check if the photo previously existed
-	rows3, err3 := db.c.Query(`select userid,path,likes,comments,date from photos where id=?`, p.ID)
+	rows3, err7 := db.c.Query(`select userid,path,likes,comments,date from photos where id=?`, p.ID)
 
-	if err3 != nil {
-		return c, p, u, err3
+	if err7 != nil {
+		return c, p, u, err7
 	}
 
 	defer rows3.Close()
 
 	for rows3.Next() {
 
-		err = rows3.Scan(&p.UserId, &p.Path, &p.Likes, &p.Comments, &p.Date)
+		err8 := rows3.Scan(&p.UserId, &p.Path, &p.Likes, &p.Comments, &p.Date)
 
-		if err != nil {
-			return c, p, u, err
+		if err8 != nil {
+			return c, p, u, err8
 		}
 
 	}
 
-	err = rows3.Err()
-	if err != nil {
-		return c, p, u, err
+	err9 := rows3.Err()
+	if err9 != nil {
+		return c, p, u, err9
 	}
 
 	if p.Path == "" {
@@ -107,9 +107,9 @@ func (db *appdbimpl) UncommentPhoto(c Comment, p Photo, u User) (Comment, Photo,
 	// Here we cast the comments to "raw format" { 1 Content ...} --> json.Unmarshal
 
 	in := []byte(p.Comments)
-	err = json.Unmarshal(in, &castComments)
-	if err != nil {
-		return c, p, u, err
+	err10 := json.Unmarshal(in, &castComments)
+	if err10 != nil {
+		return c, p, u, err10
 	}
 
 	for i := 0; i < len(castComments); i++ {
@@ -119,23 +119,23 @@ func (db *appdbimpl) UncommentPhoto(c Comment, p Photo, u User) (Comment, Photo,
 	}
 
 	// Now, in newComments we have only the comments we want. We have to store them as {"ID": 1, "Content": ...} --> json.Marshal
-	saveComments, err := json.Marshal(newComments)
-	if err != nil {
-		return c, p, u, err
+	saveComments, err11 := json.Marshal(newComments)
+	if err11 != nil {
+		return c, p, u, err11
 	}
 	p.Comments = string(saveComments)
 
-	res, err = db.c.Exec(`UPDATE photos SET path=?,comments=?,date=?,userid=?,likes=? WHERE id=?`,
+	res2, err12 := db.c.Exec(`UPDATE photos SET path=?,comments=?,date=?,userid=?,likes=? WHERE id=?`,
 		p.Path, p.Comments, p.Date, p.UserId, p.Likes, p.ID)
-	if err != nil {
-		return c, p, u, errors.New("Error in: " + fmt.Sprint(res))
+	if err12 != nil {
+		return c, p, u, errors.New("Error in: " + fmt.Sprint(res2))
 	}
 
 	// Here we update the information of the photo on "raw format" { 1 Content ...} --> json.Unmarshal
 	in2 := []byte(u.Photos)
-	err = json.Unmarshal(in2, &castPhotos)
-	if err != nil {
-		return c, p, u, err
+	err13 := json.Unmarshal(in2, &castPhotos)
+	if err13 != nil {
+		return c, p, u, err13
 	}
 
 	for i := 0; i < len(castPhotos); i++ {
@@ -143,16 +143,16 @@ func (db *appdbimpl) UncommentPhoto(c Comment, p Photo, u User) (Comment, Photo,
 			castPhotos[i].Comments = p.Comments
 		}
 	}
-	savePhotos, err := json.Marshal(castPhotos)
-	if err != nil {
-		return c, p, u, err
+	savePhotos, err14 := json.Marshal(castPhotos)
+	if err14 != nil {
+		return c, p, u, err14
 	}
 	u.Photos = string(savePhotos)
 
-	res, err = db.c.Exec(`UPDATE users SET name=?,profilepic=?,followers=?,banned=?,photos=? WHERE id=?`,
+	res3, err15 := db.c.Exec(`UPDATE users SET name=?,profilepic=?,followers=?,banned=?,photos=? WHERE id=?`,
 		u.Name, u.ProfilePic, u.Followers, u.Banned, u.Photos, u.ID)
-	if err != nil {
-		return c, p, u, err
+	if err15 != nil {
+		return c, p, u, errors.New("Error in " + fmt.Sprint(res3))
 	}
 
 	return c, p, u, nil
